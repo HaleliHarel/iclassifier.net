904600d9e101d6df13429dd469cd463a86e036fe
HaleliHarel <Haleli.Harel@mail.huji.ac.il>
2026-03-29 18:13:27 +0200

    Rename nav button from App (Dev) to App (test)

--- iclassifier_testing_clone/netlify/functions/api.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import serverless from 'serverless-http';
-import { createServer } from '../../server/index.js';
-
-// Create the Express app
-const app = createServer();
-
-// Export the serverless handler
-export const handler = serverless(app);
